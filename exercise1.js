81d7898e510cdfb415ff6bbe3067dcbb24863b57
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-30 14:48:38 -0300

    exercicio 1

--- exercise1.js
+++ exercise1.js
@@ -1,12 +1,18 @@
 function exercise1(n1, n2, n3) {
   const promise = new Promise((resolve, reject) => {
-    if(typeof (n1 || n2 || n3) !== 'number' ) {
-      reject("Informe apenas números")
+    if((typeof (n1 && n2 && n3)) !== 'number') {
+      return reject("Informe apenas números")
     };
-    resolve((n1 + n2) * n3)
+
+    const resultado = (n1 + n2) * n3
+    if (resultado < 50) return reject('Valor muito baixo');
+    
+    resolve(resultado);
   })
   return promise;
 }
-exercise1(2, 3, '4')
+
+exercise1(2, 3, 40)
   .then((response) => console.log(response))
   .catch((erro) => console.log(erro));
+
