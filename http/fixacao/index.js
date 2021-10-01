a8eb55d1340c2acf02ede521a3e979cba3153b87
Arthur Hermann <arthurhermannc@gmail.com>
2021-10-01 13:37:42 -0300

    preparacao revisao http

---
+++ http/fixacao/index.js
@@ -0,0 +1,21 @@
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+// 1 - CRIE UMA ROTA QUE BUSQUE TODOS OS DRINKS
+
+// 2 - CRIE UMA ROTA QUE RETORNE OS DRINKS FILTRADOS POR NOME, PREÇO MÁXIMO E PREÇO MÍNIMO. ESSES PARÂMETROS DEVEM SER RECEBIDOS ATRAVÉS DA QUERY STRING
+
+// 3 - CRIE UMA ROTA QUE BUSQUE O DRINK PELO ID
+
+// 4 - CRIE UMA ROTA QUE ADICIONE UM NOVO DRINK
+
+// 5 - CRIE UMA ROTA QUE ATUALIZE UM DRINK PELO ID
+
+// 6 - CRIE UMA ROTA QUE DELETE UM DRINK PELO ID
